48e430f2e6dacfd504d9226b2564399fabcb5409
simonzouvala <445475@mail.muni.cz>
2021-01-17 22:10:12 +0100

    init commit

---
+++ app/src/main/java/com/example/safejoke/model/JokeModel.kt
@@ -0,0 +1,7 @@
+package com.example.safejoke.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class JokeModel(application: Application) : AndroidViewModel(application) {
+}
